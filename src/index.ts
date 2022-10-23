c419a5c077a62f3899f5a210ab2ba2fe3691feb7
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-10-23 21:48:58 +0700

    added optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-function greet(name: string | null | undefined) {
-  if (name) {
-    console.log(name.toUpperCase());
-  } else {
-    console.log("Hola!");
-  }
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(undefined);
+let customer = getCustomer(1);
+// if (customer !== null && customer !== undefined)
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// Customers?.[0]
+
+// Optional call operators
+let log: any = null;
+log?.("a");
